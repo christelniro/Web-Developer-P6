8641c29088f401a3273b5e8a1312f535af961596
christelnsewosolo <84200905+christelnsewosolo@users.noreply.github.com>
2022-02-05 02:26:30 +0100

    express mangoose installé

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,27 @@
 const express = require('express');
 
+const mongoose = require('mongoose');
+
+
+
+
+mongoose.connect(
+    'mongodb+srv://niroc:Cn172617@cluster0.penuk.mongodb.net/project0?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
 const app = express();
 
 app.use((req, res) => {
-    res.json({ message: 'Votre requête a bien été reçue !' });
+    res.json({ message: '' });
 });
 
+
+module.exports = mongoose;
 
 module.exports = app;
