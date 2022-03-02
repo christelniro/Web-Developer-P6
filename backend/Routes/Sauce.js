50308ab91f1c6b92a21e9dd1df7afd71e2ff2917
christelnsewosolo <84200905+christelnsewosolo@users.noreply.github.com>
2022-03-02 17:07:05 +0100

    modification

--- backend/Routes/Sauce.js
+++ backend/Routes/Sauce.js
@@ -3,5 +3,18 @@ const express = require('express');
 const router = express.Router();
 
 
+const auth = require('../middleware/auth');
+
+
+//Create and modify sauces
+router.get('/', auth, saucesCtrl.getAllSauces);
+router.post('/', auth, multer, saucesCtrl.createSauce);
+router.get('/:id', auth, saucesCtrl.getOneSauce);
+router.put('/:id', auth, multer, saucesCtrl.modifySauce);
+router.delete('/:id', auth, saucesCtrl.deleteSauce);
+
+
+//Add and remove sauces review
+router.post('/:id/like', auth, likesCtrl.addLikeOrDislike);
 
 module.exports = router;
