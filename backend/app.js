24c15e33dd1ba8905d750d3791746a7915a2c417
christelnsewosolo <84200905+christelnsewosolo@users.noreply.github.com>
2022-03-02 11:32:24 +0100

    creation nouveau dossier

--- backend/app.js
+++ backend/app.js
@@ -21,6 +21,21 @@ mongoose.connect(
     .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 
+// Middleware Header pour contourner les erreurs en débloquant certains systèmes de sécurité CORS, afin que tout le monde puisse faire des requetes depuis son navigateur
+app.use((req, res, next) => {
+    // on indique que les ressources peuvent être partagées depuis n'importe quelle origine
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // on indique les entêtes qui seront utilisées après la pré-vérification cross-origin afin de donner l'autorisation
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    // on indique les méthodes autorisées pour les requêtes HTTP
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    // on autorise ce serveur à fournir des scripts pour la page visitée
+    res.setHeader('Content-Security-Policy', "default-src 'self'");
+    next();
+});
+
+
+
 
 app.use((req, res,) => {
     res.json({ message: 'test' });
@@ -29,8 +44,10 @@ app.use((req, res,) => {
 
 
 
-
-
+// Va servir les routes dédiées aux utilisateur
+app.use('/api/auth', User);
+// Va servir les routes dédiées aux sauces
+app.use('/api/sauces', Sauce)
 
 
 
